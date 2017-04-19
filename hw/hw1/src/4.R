01b6dd7a61ed7930e2492fe66c4c0ee54dd9cf39
Arthur Lui <luiarthur@gmail.com>
2017-04-19 09:30:39 -0700

    yes

--- hw/hw1/src/4.R
+++ hw/hw1/src/4.R
@@ -12,7 +12,7 @@ num_cov_fn <- length(cov_fn)
 ### phi for which corr is 0.05 at distance 1 and variance=1, nu=1 ###
 phi <- lapply(cov_fn, function(x) find_phi(x, r=.05, d=1, nu=1)$root)
 phi$wave <- 2E-3 # .33
-phi$rational_quad <- .12# .229
+#phi$rational_quad <- .12# .229
 wierd <- c("wave", "rational_quad")
 
 ### covariance fn for this hw ###
@@ -23,7 +23,8 @@ names(my_cov_fn) <-  names(phi)
 
 ### length of GP vector ###
 n <- 100
-x <- seq(0,1,len=n)
+#x <- seq(0,1,len=n)
+x <- seq(-2,2,len=n)
 
 ### PLOTS ###
 gp <- sapply(my_cov_fn, function(cf) gp_sim(x, cov_fn=cf,eps=1E-10))
