0cb951ab19c5c246ef5d8614fb78edcbc518b85e
Arthur Lui <luiarthur@gmail.com>
2017-05-11 18:05:53 -0700

    yes

--- hw/hw3/src/explore.R
+++ hw/hw3/src/explore.R
@@ -43,11 +43,13 @@ stopifnot(all_states[which(all_states$State == "California"), "Counts"] > 100)
 ### Make sure I'm doing this right ###
 #`State.Name` IN ("Washington","Oregon")
 #`State.Name` IN ("Pennsylvania", "Ohio", "New York")
+#`State.Name` IN ("Wyoming", "Idaho", "Montana", "Utah", "Colorado","Nevada","Arizona")
+# `State.Name` IN ("Arizona","Texas","New Mexico")
 # Matt Heiner got 112, I'm getting 115
 sqldf('
   SELECT SUM(COUNTS) FROM all_states
   WHERE
-    `State.Name` IN ("Wyoming", "Idaho", "Montana", "Utah", "Colorado","Nevada","Arizona")
+    `State.Name` IN ("Tennessee", "Kentucky", "Virginia", "North Carolina")
 ')
 
 
@@ -82,12 +84,13 @@ ca_all <- sqldf('
   WHERE 
     ca.`State.Name` = site_ca.`State.Name` AND
     ca.`County.Name` = site_ca.`County.Name` AND
-    ca.`Site.Num` = site_ca.`Site.Number`
+    ca.`Site.Num` = site_ca.`Site.Number` AND
+    site_ca.Elevation > 10
 ')
 
 ### Maps ###
-s <- ca[c('Latitude', 'Longitude')]
-counties <- as.character(unique(ca$County.Name))
+s <- ca_all[c('Latitude', 'Longitude')]
+counties <- as.character(unique(ca_all$County.Name))
 state_county <- tolower(paste0('California,', counties))
 
 ### Check all counties in data can be mapped ###
@@ -104,14 +107,15 @@ county_means <- sqldf('
   SELECT 
     `County.Name` AS cname, 
     AVG(`Arithmetic.Mean`) AS cmean, 
-    COUNT(`County.Name`) AS ccount
-  FROM ca 
+    COUNT(`County.Name`) AS ccount,
+    Elevation
+  FROM ca_all 
   GROUP BY `County.Name`
 ')
 
 plot.per.county(log(county_means$cmean), 'california', county_means$cname, 
                 levels=7, measure='log county means', text.name=FALSE)
-plot.per.county(ca_all$Ele, 'california', county_means$cname, 
+plot.per.county(county_means$Elevation, 'california', county_means$cname, 
                 levels=7, measure='Count Elevation', text.name=FALSE)
 
 
@@ -128,9 +132,9 @@ my.pairs(vars)
 
 vars <- cbind(ca_all$Arithmetic.Mean, 
               ca_all$Lat,
-              sqrt(ca_all$Lon+124.3),
-              ca_all$Elevation)
-colnames(vars) <- c('Mean', 'Lat', 'Lon', 'Elevation')
+              ca_all$Lon,
+              log(ca_all$Elevation))
+colnames(vars) <- c('Mean', 'Lat', 'Lon', 'log(Elevation)')
 my.pairs(vars)
 
 ### Variogram ###
@@ -139,27 +143,34 @@ plot(variog(data=ca_all$Arithmetic.Mean, coords=s), type='b')
 
 ### Transform and detrend ###
 y <- ca_all$Arithmetic.Mean
-lon <- ca_all$Lon
+alt <- ca_all$Elevation
+plot(alt, y)
+plot(log(alt), y)
+#lon <- ca_all$Lon
+#tlon <- log(lon-min(lon)+1)
+#plot(lon, y)
+#plot(tlon, y)
+#mod <- lm(y ~ tlon)
 
-tlon <- log(lon-min(lon)+1)
-
-plot(lon, y)
-plot(tlon, y)
-mod <- lm(y ~ tlon)
+mod <- lm(y ~ log(alt))
 abline(mod)
 
-vars.new <- cbind(y-tlon*mod$coef[2], #mod$resid
+vars.new <- cbind(y - cbind(1, log(alt)) %*% mod$coef, #mod$resid
                   ca_all$Lat, ca_all$Lon,
-                  ca_all$Elev)
-colnames(vars.new) <- c('Mean.detrended', 'Lat', 'Lon', 'Elevation')
+                  log(alt))
+colnames(vars.new) <- c('Mean.detrended', 'Lat', 'Lon', 'log(Elevation)')
 my.pairs(vars.new)
 
 par(mfrow=c(2,1))
-plot(variog(data=ca_all$Arithmetic.Mean, coords=s), type='b', main='Semi-variogram')
-plot(variog(data=mod$resid, coord=s), type='b', main='Semi-variogram after detrend')
+plot(variog(data=ca_all$Arithmetic.Mean, coords=s),
+     type='b', main='Semi-variogram')
+plot(variog(data=mod$resid, coord=s),
+     type='b', main='Semi-variogram after detrend')
 par(mfrow=c(1,1))
 
 par(mfrow=2:1)
-plot(variog4(data=ca_all$Arithmetic.Mean, coords=s), type='b', main='Semi-variogram')
-plot(variog4(data=mod$resid, coord=s), type='b', main='Semi-variogram after detrend')
+plot(variog4(data=ca_all$Arithmetic.Mean, coords=s), type='b')
+title(main='Semi-variogram')
+plot(variog4(data=mod$resid, coord=s), type='b')
+title(main='Semi-variogram after detrend')
 par(mfrow=c(1,1))
