8f44b7367104ce302e5517610e902697af604a5e
Arthur Lui <luiarthur@gmail.com>
2017-05-19 15:01:47 -0700

    yes

--- spatialScala/R/GP.R
+++ spatialScala/R/GP.R
@@ -1,26 +1,30 @@
-GP <- function(y, X, loc, stepSigCov, a_tau, b_tau, 
-               a_sig, b_sig, a_phi, b_phi, 
-               a_nu, b_nu, B, burn, print_every=0,
-               cov_fn="matern") {
-  #' rBreeze_repeat
-  #' @export
-  #rscala::scalaEval(s,'rBreeze.repeat(@{n})')
-  #rscala::s$do()
-  rscala::s rscala::%~% '
-    spatialScala.GPs.fitMatern(
-      DenseVector(@{y}),
-      DenseMatrix(@{X}),
-      DenseMatrix(@{loc}),
-      DenseMatrix(@{stepSigCov}),
-      @{a_tau},
-      @{b_tau},
-      @{a_sig},
-      @{b_sig},
-      @{a_phi},
-      @{b_phi},
-      @{a_nu},
-      @{b_nu},
-      @{B}, @{burn}, @{print_every}
-    )
-  '
-}
+GP <- rscala::s$def(y,X) %~% '
+  println("Hi")
+'
+
+#GP <- function(y, X, loc, stepSigCov, a_tau, b_tau, 
+#               a_sig, b_sig, a_phi, b_phi, 
+#               a_nu, b_nu, B, burn, print_every=0,
+#               cov_fn="matern") {
+#  #' rBreeze_repeat
+#  #' @export
+#  #rscala::scalaEval(s,'rBreeze.repeat(@{n})')
+#  #rscala::s$do()
+#  rscala::scalap '
+#    spatialScala.GPs.fitMatern(
+#      DenseVector(@{y}),
+#      DenseMatrix(@{X}),
+#      DenseMatrix(@{loc}),
+#      DenseMatrix(@{stepSigCov}),
+#      @{a_tau},
+#      @{b_tau},
+#      @{a_sig},
+#      @{b_sig},
+#      @{a_phi},
+#      @{b_phi},
+#      @{a_nu},
+#      @{b_nu},
+#      @{B}, @{burn}, @{print_every}
+#    )
+#  '
+#}
