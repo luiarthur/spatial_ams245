ea563aa5abd2f6c13eda32f5182d18dc826adb19
Arthur Lui <luiarthur@gmail.com>
2017-05-19 15:37:48 -0700

    yes

--- spatialScala/R/GP.R
+++ spatialScala/R/GP.R
@@ -41,6 +41,7 @@ GP <- function(y, X, loc, stepSigCov, a_tau, b_tau,
                a_sig, b_sig, a_phi, b_phi, 
                a_nu, b_nu, B, burn, print_every=0,
                cov_fn="matern") {
+  #' GP Function
   #' @export
   #rscala::scalaEval(s,'rBreeze.repeat(@{n})')
   #rscala::s$do()
