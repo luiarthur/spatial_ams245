d67c6d17a8d5ee107b587f4f5ca10e8011461318
Arthur Lui <luiarthur@gmail.com>
2017-05-19 15:28:53 -0700

    yes

--- spatialScala/R/GP.R
+++ spatialScala/R/GP.R
@@ -9,30 +9,32 @@ GP <- function(y, X, loc, stepSigCov, a_tau, b_tau,
              a_nu, b_nu, B, burn, print_every=0, cov_fn="matern")
 }
 
-gp_wrapper <- rscala::"%~%"(
-  s$def(y, X, loc, stepSigCov, a_tau, b_tau, 
-        a_sig, b_sig, a_phi, b_phi, 
-        a_nu, b_nu, B, burn, print_every=0,
-        cov_fn="matern"), '
-        val n = y.size
-        val p = 2
-        val k = 3
-        spatialScala.GPs.fitMatern(
-          breeze.linalg.DenseVector(y),
-          new breeze.linalg.DenseMatrix(k,n,X).t,
-          new breeze.linalg.DenseMatrix(p,n,loc).t,
-          new breeze.linalg.DenseMatrix(4,4,stepSigCov),
-          a_tau,
-          b_tau,
-          a_sig,
-          b_sig,
-          a_phi,
-          b_phi,
-          a_nu},
-          b_nu},
-          B, burn, print_every
-        )
-')
+gp_wrapper <- s$def(y, X, loc, stepSigCov,
+                    a_tau, b_tau, 
+                    a_sig, b_sig,
+                    a_phi, b_phi, 
+                    a_nu, b_nu,
+                    B, burn, print_every=0,
+                    cov_fn="matern"), %~% '
+  val n = y.size
+  val p = 2
+  val k = 3
+  spatialScala.GPs.fitMatern(
+    breeze.linalg.DenseVector(y),
+    new breeze.linalg.DenseMatrix(k,n,X).t,
+    new breeze.linalg.DenseMatrix(p,n,loc).t,
+    new breeze.linalg.DenseMatrix(4,4,stepSigCov),
+    a_tau,
+    b_tau,
+    a_sig,
+    b_sig,
+    a_phi,
+    b_phi,
+    a_nu},
+    b_nu},
+    B, burn, print_every
+  )
+'
 
 
 #GP <- function(y, X, loc, stepSigCov, a_tau, b_tau, 
