ba63e8752c78e11b44dec07ed36dff5c3141c93c
Arthur Lui <luiarthur@gmail.com>
2017-05-19 15:04:12 -0700

    yes

--- spatialScala/R/GP.R
+++ spatialScala/R/GP.R
@@ -6,7 +6,7 @@ GP <- function(y, X, loc, stepSigCov, a_tau, b_tau,
   #' @export
   #rscala::scalaEval(s,'rBreeze.repeat(@{n})')
   #rscala::s$do()
-  rscala::s rscala::"%~%" '
+  rscala::"%~%"(rscala::s, '
     spatialScala.GPs.fitMatern(
       DenseVector(@{y}),
       DenseMatrix(@{X}),
@@ -22,5 +22,5 @@ GP <- function(y, X, loc, stepSigCov, a_tau, b_tau,
       @{b_nu},
       @{B}, @{burn}, @{print_every}
     )
-  '
+  ')
 }
