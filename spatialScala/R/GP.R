90f5153b7d88d7ecef0295c299288b873ef69f7d
Arthur Lui <luiarthur@gmail.com>
2017-05-19 11:15:25 -0700

    yes

--- spatialScala/R/GP.R
+++ spatialScala/R/GP.R
@@ -1,6 +1,26 @@
-GP <- function(n, cov_fn="exp") {
+GP <- function(y, X, s, stepSigCov, a_tau, b_tau, 
+               a_sig, b_sig, a_phi, b_phi, 
+               a_nu, b_nu, B, burn, print_every=0,
+               cov_fn="matern") {
   #' rBreeze_repeat
   #' @export
   #rscala::scalaEval(s,'rBreeze.repeat(@{n})')
   #rscala::s$do()
+  rscala::scalaEval(s, '
+    spatialScala.GPs.fitMatern(
+      DenseVector(@{y}),
+      DenseMatrix(@{X}),
+      DenseMatrix(@{s}),
+      DenseMatrix(@{stepSigCov}),
+      @{a_tau},
+      @{b_tau},
+      @{a_sig},
+      @{b_sig},
+      @{a_phi},
+      @{b_phi},
+      @{a_nu},
+      @{b_nu},
+      @{B}, @{burn}, @{print_every}
+    )
+  ')
 }
