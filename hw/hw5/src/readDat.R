df3cb298af864a5a33909f0fbf6dc17b7c509e90
Arthur Lui <luiarthur@gmail.com>
2017-06-03 15:35:30 -0700

    yes

--- hw/hw5/src/readDat.R
+++ hw/hw5/src/readDat.R
@@ -9,7 +9,11 @@ library(maps)   # map
 source("../../hw3/src/plotPerCounty.R")
 
 ### Entire Data ###
-dat <- read.csv('../../hw3/dat/annual_all_2015.csv')
+#dat <- read.csv('../../hw3/dat/annual_all_2015.csv')
+#site <- read.csv('../../hw3/dat/aqs_sites.csv')
+NO2 <-  read.csv('../dat/daily/daily_NO2_2015.csv')
+ozone <-  read.csv('../dat/daily/daily_ozone_2015.csv')
+pm25 <-  read.csv('../dat/daily/daily_pm25_2015.csv')
 site <- read.csv('../../hw3/dat/aqs_sites.csv')
 
 # dim = 135, 56
@@ -17,7 +21,7 @@ ca <- sqldf('
   SELECT dat.*, site.Elevation FROM dat LEFT JOIN site
   WHERE
     dat.`State.Name` IN("California") AND
-    dat.`Parameter.Name`="Ozone" AND
+    dat.`Parameter.Name` IN("Ozone") AND
     dat.`Sample.Duration`="8-HR RUN AVG BEGIN HOUR" AND
     dat.`Pollutant.Standard`="Ozone 8-Hour 2008" AND
     dat.`Event.Type` IN("No Events",
